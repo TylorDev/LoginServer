590f728712333782196e28ef7393113a01e47710
TylorDev <jhontmg26@gmail.com>
2024-03-14 21:06:50 -0300

    no tasks

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
-import taskRoutes from "./routes/tasks.routes.js";
+
 import cors from "cors";
 const app = express();
 app.use(
@@ -16,5 +16,5 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", authRoutes);
-app.use("/api", taskRoutes);
+
 export default app;
--- src/schemas/task.schema.js
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const createTaskSchema = z.object({
-  title: z.string({ required_error: "Title is required" }),
-  description: z.string({ required_error: "Description is required" }),
-  date: z.string().datetime().optional(),
-});
